efbfe6f1c31391fa04ecfda7f49409f0c538266a
Annabelle <32454421+anna791p@users.noreply.github.com>
2018-05-23 07:03:04 +0200

    form implemented on contact, php set up

---
+++ sendMail.php
@@ -0,0 +1,27 @@
+
+<?php
+
+$to      = 'hello@annabellegoldsworthy.com'; //one.com email, or domain email
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$sub = $_POST['subject'];
+$body = $_POST['message'];
+
+
+$subject = 'New Enquiry';
+
+$message = "New enquiry from " ;
+$headers = 'From: hello@annabellegoldsworthy.com' . "\r\n" . //why do we put in our email here? or do we need to?
+    'Reply-To: hello@annabellegoldsworthy.com' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+if(mail($to, $subject, $message, $headers)){
+    header("Location: index.html");
+} else {
+    echo "Failure to send"
+}
+    ;
+?>
+
+<!-- https://stackoverflow.com/questions/5335273/how-to-send-an-email-using-php -->
