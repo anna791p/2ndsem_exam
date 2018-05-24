35f99da2c095b64206ff40e011410423ac89ee95
ekaterinagp <katya.koriakina@gmail.com>
2018-05-24 09:22:58 +0200

    test video into img

--- menu.js
+++ menu.js
@@ -1,21 +1,30 @@
+let menuOpen = false;
+let menuIcon = document.querySelector(".menuIcon")
+let menu = document.querySelector(".menu");
+let bars = menuIcon.querySelectorAll("rect");
+let appbar = document.querySelector(".appbar")
+menuIcon.addEventListener('click', toggleMenu);
 
-  let menuOpen = false;
-  let menuIcon = document.querySelector(".menuIcon")
-  let menu = document.querySelector(".menu");
- let bars = menuIcon.querySelectorAll("rect");
-    let appbar = document.querySelector(".appbar")
-  menuIcon.addEventListener('click', toggleMenu);
+function toggleMenu() {
+  menuOpen = !menuOpen;
+  bars[0].classList.toggle("rotateDown");
+  bars[1].classList.toggle("fadeOut");
+  bars[2].classList.toggle("rotateUp");
+  menu.classList.toggle("hidden");
 
-  function toggleMenu(){
-   menuOpen = !menuOpen;
-    bars[0].classList.toggle("rotateDown");
-    bars[1].classList.toggle("fadeOut");
-    bars[2].classList.toggle("rotateUp");
-    menu.classList.toggle("hidden");
+}
 
-  }
+// Video into pictures
+// let vidImg = document.querySelector("#vid_img");
+// let n = 1;
 
+// console.log("vidImg:" + vidImg);
+// // vidImg.src = "img/2.jpg";
 
-
-
-
+// setInterval(function() {
+//   vidImg.src = "img/" + n + ".jpg";
+//   n = n + 1;
+//   if (n > 10) {
+//     n = 1;
+//   }
+// }, 200);
