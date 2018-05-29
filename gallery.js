da97dc60a9bb6a1d2706852409d349e21b9a2eeb
ekaterinagp <katya.koriakina@gmail.com>
2018-05-29 06:47:06 +0200

    footer and categories

--- gallery.js
+++ gallery.js
@@ -15,8 +15,50 @@ let handleProjectItemHover = function(projectItemElement, option) {
   projectItemElement.children[1].style.display = option;
 }
 
+fetch("http://valsdottir.net/kea/07-cms/wordpress/wp-json/wp/v2/categories")
+  .then(e => e.json())
+  .then(buildMenu)
+
+
+function buildMenu(data) {
+  let parentElement = document.querySelector(".tags");
+  data.forEach(item => {
+    console.log(item);
+    if (item.name !== "Uncategorised") {
+      let li = document.createElement("li");
+      let a = document.createElement("a");
+      li.id = item.id;
+      a.textContent = item.name;
+      a.href = "artwork.html?category=" + item.id;
+      li.appendChild(a);
+      parentElement.appendChild(li);
+      console.log("categories:" + data);
+    }
+
+
+
+
+
+  })
+
+}
+
+let page = 1;
+let lookingForData = false;
+
+
 function fetchGallery() {
-  fetch("http://valsdottir.net/kea/07-cms/wordpress/wp-json/wp/v2/artist?_embed")
+  lookingForData = true;
+  let urlParams = new URLSearchParams(window.location.search);
+  let catid = urlParams.get("category");
+  console.log("fetchGallery: catid: ", catid);
+  let endpoint = "http://valsdottir.net/kea/07-cms/wordpress/wp-json/wp/v2/artist?_embed"
+  if (catid) {
+    let menuItemClick = document.getElementById(catid);
+    // menuItemClick.classList.add("active_cat");
+    endpoint = "http://valsdottir.net/kea/07-cms/wordpress/wp-json/wp/v2/artist?_embed" + "&categories=" + catid
+  }
+  fetch(endpoint)
     .then(e => e.json())
     .then(showGallery)
 }
